f8d23ea30b599faddadeaa778cbb9d469ae3180f
Nurul1996 <nfhasibuan@gmail.com>
2020-03-06 15:47:07 +0700

    deploy

---
+++ precache-manifest.f51c71b28676ae9ab6aec4950bfa996a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160a8bdb4689b78f785fecbbe6ca9f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947b26bc44fb0c065edd",
+    "url": "/static/css/main.b8ce7f36.chunk.css"
+  },
+  {
+    "revision": "2445fef006b568e3acb9",
+    "url": "/static/js/2.b3b8950f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b3b8950f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947b26bc44fb0c065edd",
+    "url": "/static/js/main.9558d110.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "1894ab70f2718c8b64e130704d9ff335",
+    "url": "/static/media/Cover.1894ab70.jpg"
+  },
+  {
+    "revision": "8c0e26296a6c7db7bff4d8c59037f2ea",
+    "url": "/static/media/Darah.8c0e2629.jpg"
+  },
+  {
+    "revision": "e41a3d3b1f6aed621f8a8e8deefabb8b",
+    "url": "/static/media/Dua.e41a3d3b.jpg"
+  },
+  {
+    "revision": "6ed3c202a9f7ec37000da3347ee406fb",
+    "url": "/static/media/Tiga.6ed3c202.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
